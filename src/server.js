65959be1006a45b7ee39147826acb8aeb809114f
Arjun Mayur <arjunmayur@Arjuns-MacBook-Air.local>
2017-12-09 23:01:31 -0600

    save data for individual applicant

--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
 var express = require('express')
 var firebase = require("firebase");
+require('firebase/auth');
+require('firebase/database');
 var app = express()
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
@@ -9,22 +11,22 @@ app.use(bodyParser.urlencoded({ extended: true }));
 var config = {
   apiKey: "AIzaSyAMcqmIvLgrC4gfVO6p06buBYSgD680eB0",
   authDomain: "finalcloudcomp.firebaseapp.com",
-  databaseURL: "https://finalcloudcomp.firebaseio.com",
-  storageBucket: "",
+  databaseURL: "https://finalcloudcomp.firebaseio.com"
 };
 
 firebase.initializeApp(config);
-
 
 app.listen(3000, function () {
   console.log('App listening on port 3000!')
 })
 
 app.post("/individualApp",function (req, res) {
-  	firebase.database.ref('/').set(req.body)
+	console.log("post received individual App")
+	var database = firebase.database();
+	database.ref('/Applicants').push(req.body);
 })
 
-
 app.post("/partnerApp",function (req, res) {
-   firebase.database.ref('/').set(req.body)
+   console.log("post received partnerApp")
+   var database = firebase.database();
 })
